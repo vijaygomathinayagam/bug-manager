bcbee549250869d0476e38cfc238b8714398b341
Vijay Gomathinayagam <vijay.gomathinayagam@hashouttech.com>
2020-05-19 20:58:55 +0530

    add limit, sort, column projections for getallbugs

--- backend-api/src/entities/bug.js
+++ backend-api/src/entities/bug.js
@@ -51,7 +51,11 @@ const { bugCollectionName } = require('../common').constants;
 const bugModal = mongoose.model('bug', bugSchema, bugCollectionName);
 
 const getAllBugs = async () => {
-  return await bugModal.find({});
+  return await bugModal
+    .find({})
+    .limit(10)
+    .sort({ createdAt: -1 })
+    .select({ bugId: 1, title: 1, reportedBy: 1, assignedBy: 1});
 };
 
 module.exports = {
--- backend-api/test/unit/entities/bug_test.js
+++ backend-api/test/unit/entities/bug_test.js
@@ -12,7 +12,6 @@ describe("bug entity methods", async function() {
 
         it("bug modal should return error validating invalid data", function() {
             let error = getFakeInvalidBugObjEmpty().validateSync();
-            console.log(error);
             assert.notEqual(error.errors.title, undefined);
             assert.notEqual(error.errors.bugID, undefined);
             assert.notEqual(error.errors.actualBehaviour, undefined);
@@ -30,13 +29,35 @@ describe("bug entity methods", async function() {
 
     describe("getAllBugs method", async function() {
 
-        before(function() {
-            sinon.stub(bugModal, 'find');
+        const selectStub = sinon.stub();
+        const sortStub = sinon.stub().callsFake(() => ({
+            select: selectStub,
+        }));
+        const limitStub = sinon.stub().callsFake(() => ({
+            sort: sortStub,
+        }));
+
+        before(async function() {
+            sinon.stub(bugModal, 'find').callsFake(() => ({
+                limit: limitStub,
+            }));
+            await getAllBugs();
         });
 
         it("getAllBugs should call find with empty object", async function() {
-            await getAllBugs();
             sinon.assert.calledWith(bugModal.find, {});
+        });
+
+        it("getAllBugs should call limit with 10", async function() {
+            sinon.assert.calledWith(limitStub, 10);
+        });
+
+        it("getAllBugs should call sort with updatedAt descending", async function() {
+            sinon.assert.calledWith(sortStub, { createdAt: -1 });
+        });
+
+        it("getAllBugs should call select with bugID, title, assignedTo and reportedBy columns", async function() {
+            sinon.assert.calledWith(selectStub, { bugId: 1, title: 1, reportedBy: 1, assignedBy: 1});
         });
 
         after(function() {
