7a01f4dc858c431c224ae1d0429e118d8cc7337a
Vijay Gomathinayagam <vijay.gomathinayagam@hashouttech.com>
2020-05-25 15:44:38 +0530

    route must be added for get bug api

--- backend-api/src/routes/bug.js
+++ backend-api/src/routes/bug.js
@@ -1,8 +1,13 @@
 const { validSessionMiddleware } = require('../middlewares/session-check');
 const { reportedByUserCheckMiddleWare } = require('../middlewares/bug');
-const { getAllBugsHandler, deleteBugHandler } = require('../handlers/bug');
+const {
+    getAllBugsHandler,
+    getBugHandler,
+    deleteBugHandler
+} = require('../handlers/bug');
 
 module.exports = (apiRouter) => {
     apiRouter.get('/bugs', validSessionMiddleware, getAllBugsHandler);
+    apiRouter.get('/bugs/{:bugID}', validSessionMiddleware, getBugHandler);
     apiRouter.delete('/bugs/{:bugID}', validSessionMiddleware, reportedByUserCheckMiddleWare, deleteBugHandler);
 };
