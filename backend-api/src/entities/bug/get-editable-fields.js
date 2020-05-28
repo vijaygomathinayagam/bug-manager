2468873786cfbfddcbc0b0ec0267f3940568c6a8
Vijay Gomathinayagam <vijay.gomathinayagam@hashouttech.com>
2020-05-28 10:26:50 +0530

    api test for get user associations bug editable fields must be added

--- backend-api/src/entities/bug/get-editable-fields.js
+++ backend-api/src/entities/bug/get-editable-fields.js
@@ -1,5 +1,5 @@
 const userBugAssociationBugEditableFields = require('../../../resources/bug_editable_fields.json');
 
 module.exports = async function() {
-    return userAssociationBugEditableFields;
+    return userBugAssociationBugEditableFields;
 }
