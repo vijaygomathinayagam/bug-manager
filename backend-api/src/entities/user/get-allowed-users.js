d679440be2e4d38ef1b7e529d876489b0454492f
Vijay Gomathinayagam <vijay.gomathinayagam@hashouttech.com>
2020-06-13 17:35:07 +0530

    get allowed users entity method has to be implemented

--- backend-api/src/entities/user/get-allowed-users.js
+++ backend-api/src/entities/user/get-allowed-users.js
@@ -1,5 +1,5 @@
 const allowedUsersList = require(`../../../resources/${process.env.Allowed_Users_Filename}`);
 
-module.exports = (userEmail) => {
-    return allowedUsersList.includes(userEmail);
+module.exports = async function() {
+    return allowedUsersList;
 }
